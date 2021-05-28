86aadbf9a34681d08010a048b8a0c797d5a58aed
vinicius-ssantos <viniciusolicveira@gmail.com>
2021-05-28 12:49:59 -0300

    Atualizacao

--- src/JAVA/ex/_1094/Main.java
+++ src/JAVA/ex/_1094/Main.java
@@ -2,8 +2,6 @@ package JAVA.ex._1094;
 
 import java.util.Locale;
 import java.util.Scanner;
-
-
 class Main {
     public static void main(String[] args) {
         int total = 2;
@@ -18,7 +16,6 @@ class Main {
         for (int x = 0; x < entrada; x++) {
             int quantidade = scan.nextInt();
             char[] tipo =  Character.toChars((scan.next().charAt(0)));
-
             if (tipo[0]=='\u0043'){
                 totalDeCoelhos += quantidade;
                 total += quantidade;
@@ -33,10 +30,9 @@ class Main {
         double porC = (totalDeCoelhos / total)*100;
         double porR = (totalDeRatos / total)*100;
         double porS = (totalDeSapos / total)*100;
-        String porcentagemC = String.format(Locale.UK, "Percentual de coelhos: %.2f %c ", porC, por);
-        String porcentagemR = String.format(Locale.UK, "Percentual de ratos: %.2f %c ", porR, por);
-        String porcentagemS = String.format(Locale.UK, "Percentual de sapos: %.2f %c ", porS, por);
-
+        String porcentagemC = String.format(Locale.UK, "Percentual de coelhos: %.2f %c", porC, por);
+        String porcentagemR = String.format(Locale.UK, "Percentual de ratos: %.2f %c", porR, por);
+        String porcentagemS = String.format(Locale.UK, "Percentual de sapos: %.2f %c", porS, por);
         System.out.printf("Total: %d cobaias\n", total);
         System.out.printf("Total de coelhos: %.0f\n", totalDeCoelhos);
         System.out.printf("Total de ratos: %.0f\n", totalDeRatos);
@@ -44,8 +40,6 @@ class Main {
         System.out.println(porcentagemC);
         System.out.println(porcentagemR);
         System.out.println(porcentagemS);
-
-
     }
 }
 
